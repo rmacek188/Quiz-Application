902f221dd711efab3aac378d3502027ce9ad2992
rmacek188 <33817052+rmacek188@users.noreply.github.com>
2020-08-04 15:22:20 -0400

    Create store.js

---
+++ store.js
@@ -0,0 +1,169 @@
+const STORE = {
+  questions: [//1
+    {
+      question: "To open the gate to Moria, Gandalf says the Elvish word for __________.",
+      options: [
+        "Enter", 
+        "Friend", 
+        "Dwarf", 
+        "Hello"
+      ],
+      answer: "Friend"
+    },
+    //2
+    {
+      question: "At the beginning of 'The Fellowship of the Ring', Bilbo is celebrating his birthday. How old is he?",
+      options: [
+        "88",
+        "93", 
+        "120",
+        "111"
+      ],
+      answer: "111"
+    },
+    //3
+    {
+      question: "In Mordor, what kind of food does Frodo say he can't remember the taste of?",
+      options: [
+        "Bread", 
+        "Cheese", 
+      "Strawberries", 
+        "Mushrooms"
+      ],
+      answer: "Strawberries"
+    },
+    //4
+    {
+      question: "Who became King of Rohan after Theoden met his demise in battle?", 
+      options: [
+        "Eowyn", 
+        "Eomer", 
+        "Grimer", 
+        "Eorl"],
+      answer: "Eomer"
+    },
+    //5
+    {
+      question: "Where does the Fellowship of the Ring break up?",
+      options: [
+        "Moria", 
+        "Mirkwood", 
+        "Isengard", 
+        "Amon Hen"
+      ],
+      answer: "Amon Hen"
+    },
+    //6
+    {
+      question: "How many wizards are there in Middle Earth?",
+      options: [
+        "1", 
+        "2", 
+        "3", 
+        "5"
+      ],
+      answer: "5"
+    },
+    //7
+    {
+      question: "Who rides ahead with Frodo to Rivendell after he is hit at Weathertop?",
+      options: [
+        "Arwen", 
+        "Aragorn", 
+        "Eowyn", 
+        "Gandalf"
+      ],
+      answer: "Arwen"
+    },
+    //8
+    {
+      question: "What is the name of the volcano that the Ring is thrown into?", 
+      options: [
+        "Osigiliath", 
+        "Cirith Ungul", 
+        "Minas Tirith", 
+        "Barad Dur"
+      ],
+      answer: "Barad Dur"
+    },
+    //9
+    {
+      question: "What is Samwise's last name?",
+      options: [
+        "Gaffer", 
+        "Baggins", 
+      "Bronzebeard", 
+        "Gamgee"
+      ], 
+      answer: "Gamgee"
+    },
+    //10
+    {
+      question: "What is the name of the local pub in Hobbiton?",
+      options: [
+        "The Black  Boar", 
+        "The Prancing   Pony", 
+        "Goldshire  Tavern", 
+        "The Green  Dragon"
+      ],
+      answer: "The Green Dragon"
+    },
+    //11
+    {
+      question: "What is Legolas' last name?",
+      options: [
+        "Stormborn", 
+        "Lightfeet", 
+        "Greenleaf", 
+        "Celebron"
+      ],
+      answer: "Greenleaf"
+    },
+    //12
+    {
+      question: "What causes Merry and Pippin to grow taller than other hobbits?", 
+      options: [
+        "The magic spells of treebeard", 
+        "The Water in Fangorn", 
+        "The sorcery of the White Wizard", 
+        "The food of the Ents"
+      ],
+      answer: "The Water in Fangorn"
+    },
+    //13
+    {
+      question: "What does Aragorn say right before charging into the last battle in Mordor?",
+      options: [
+        "It is not this day!", 
+        "Hope conquers all!", 
+        "Evil will not rule us!", 
+        "For Frodo!"
+      ],
+      answer: "For Frodo!"
+    },
+    //14
+    {
+      question: "Which of the following areas in Middle Earth has Legolas NOT been to?",
+      options: [
+        "Isengard", 
+        "Rohan", 
+        "The Shire", 
+        "Helm's Deep"
+      ],
+      answer: "The Shire"
+    },
+    //15
+    {
+      question: "What feature do Female Dwarves share with Male Dwarves?",
+      options: [
+        "Beards", 
+      "Adam's Apples", 
+        "Muscles", 
+        "Hairy Chests"
+      ],
+      answer: "Beards"
+    }
+  ],
+  currentQuestion: 0,
+  score: 0
+};
